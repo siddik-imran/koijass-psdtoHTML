0d4d9f4f4dba95e6c8c57622d5b78cfacd7e013d
Imran Hasan <59974226+siddik-imran@users.noreply.github.com>
2020-01-20 12:06:30 +0600

    js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+
+        $('.counter').counterUp({
+            delay: 10,
+            time: 5000
+        });
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
